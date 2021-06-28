ac492965864e5cb5d1378345576b958f057ca392
Florian Brandes <florian.brandes@posteo.de>
2021-06-28 21:51:59 +0200

    add config parser

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gador/ipfs-tag
+
+go 1.16
+
+require (
+	github.com/ilyakaznacheev/cleanenv v1.2.5
+	github.com/ipfs/go-ipfs-api v0.2.0
+)
--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"os"
 	"strings"
 
+	"github.com/ilyakaznacheev/cleanenv"
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
+type Config struct {
+	Port string `yaml:"port" env:"PORT" env-default:"5001"`
+	Host string `yaml:"host" env:"HOST" env-default:"localhost"`
+}
+
 func main() {
+	var cfg Config
+	err := cleanenv.ReadConfig("ipfs-tag.yml", &cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading configuration file: %s", err)
+	}
+
 	// Where your local node is running on localhost:5001
-	sh := shell.NewShell("localhost:5001")
+	sh := shell.NewShell(cfg.Host + ":" + cfg.Port)
 	cid, err := sh.Add(strings.NewReader("hello world!"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s", err)
